2214dc3b8fd74de4284f496b201fbead0c08ddb0
Eduardo Sampaio <edumsamp@gmail.com>
2018-02-09 16:32:14 -0200

    quase final

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,9 +16,6 @@ export class HomePage {
     this.navCtrl.push(DetailPage, {park: item});
   }
 
-  distancia = Number(this.distancia)/100
-  dist = this.distancia.toFixed(2).replace(".",",");
-
   mostrar = false;
 
   procEstacionamento(){ 
